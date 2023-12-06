cbf42e7b88ce9710b921f6005d8d5c5c87341b69
Jian Zheng <alexjianzheng@gmail.com>
2023-12-06 15:24:13 -0500

    demo setting

--- app/src/main/java/com/jianzheng/studyzero/service/OverlayService.kt
+++ app/src/main/java/com/jianzheng/studyzero/service/OverlayService.kt
@@ -7,26 +7,7 @@ import android.os.IBinder
 import android.os.SystemClock
 import android.util.Log
 import android.view.WindowManager
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewModelStoreOwner
@@ -36,8 +17,6 @@ import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 import com.jianzheng.studyzero.R
 import com.jianzheng.studyzero.cycle.MyLifecycleOwner
 import com.jianzheng.studyzero.ui.Overlay
-import com.jianzheng.studyzero.ui.ShowEsm
-import com.jianzheng.studyzero.ui.theme.StudyZeroTheme
 
 class OverlayService : Service() {
 
