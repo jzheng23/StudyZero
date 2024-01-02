a4302e27fcdf10f5f0a64a245a6cf38b20bcfd5d
Jian Zheng <alexjianzheng@gmail.com>
2024-01-02 16:30:37 -0500

    ready for build

--- app/src/main/java/com/jianzheng/studyzero/MainActivity.kt
+++ app/src/main/java/com/jianzheng/studyzero/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.core.content.ContextCompat
 import androidx.room.Room
 import com.jianzheng.studyzero.data.EsmDatabase
