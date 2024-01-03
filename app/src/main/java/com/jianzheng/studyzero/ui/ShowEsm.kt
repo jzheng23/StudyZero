bfe3fef863bd0326bdaec30e32b9002e4d8c0f45
Jian Zheng <alexjianzheng@gmail.com>
2024-01-03 10:36:22 -0500

    icon background

--- app/src/main/java/com/jianzheng/studyzero/ui/ShowEsm.kt
+++ app/src/main/java/com/jianzheng/studyzero/ui/ShowEsm.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -346,7 +347,14 @@ fun DismissButton(
     ) {
         Icon(
             imageVector = Icons.Filled.Close,
-            contentDescription = "Dismiss"
+            contentDescription = "Dismiss",
+            modifier = modifier
+                .size(48.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.surfaceVariant,
+                    shape = RoundedCornerShape(10.dp)
+                )
+
         )
     }
 }
